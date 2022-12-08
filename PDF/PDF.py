1645fa4b4ebeae2cd147f8c7ef3062514906423b
ilkaFOG <94058603+ilkaFOG@users.noreply.github.com>
2022-12-08 17:38:41 +0300

    +

--- PDF/PDF.py
+++ PDF/PDF.py
@@ -1,5 +1,5 @@
 import os
-# https://pyfpdf.readthedocs.io/en/latest/Tutorial/index.html
+
 
 from fpdf import FPDF
 from Book.book import Book
@@ -13,7 +13,7 @@ class PdfFile:
         files = os.listdir(PdfFile.OUTPUT_FOLDER)
         max_file = 0
         for file in files:
-            # https://stackoverflow.com/questions/35993549/python-parse-int-from-string
+
             current_file_number = int(file.title().upper().replace("VIVOD_(", "").replace(").PDF", ""))
             max_file = max(max_file, current_file_number)
         max_file = max_file + 1
@@ -27,7 +27,7 @@ class PdfFile:
         pdf = FPDF('P', 'mm', 'A4')
         filename = PdfFile.OUTPUT_FOLDER + "/" + PdfFile._next_file_name()
 
-        # https://stackoverflow.com/questions/67130517/fpdf-unicodeencodeerror-latin-1-codec-cant-encode-character-u2013-in-pos
+
         pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
         pdf.set_font('DejaVu', '', 14)
         pdf.add_page()
